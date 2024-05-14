67890dfe7217ab128a9158802e7d3ca9441d6133
LinusSee <linussee@web.de>
2024-05-14 16:08:38 +0200

    feat: add mock tracking server
    Add node express server that returns a station-check result
    The result is mocked depending on the orderId

---
+++ tracking-backend/index.js
@@ -0,0 +1,26 @@
+const express = require('express');
+const app = express();
+
+const port = 3000;
+
+
+app.use(express.json());
+
+app.post('/station/check', (req, res) => {
+    // For "unsuccessful ID" send unsuccessful event
+    if(req.body.orderId === 'B_2663588') {
+        res.send({
+            "isAtCorrectStation": false,
+            "expectedStation": 2
+        });
+    } else {
+        res.send({
+            "isAtCorrectStation": true,
+            "expectedStation": 1
+        });
+    }
+});
+
+app.listen(port, () => {
+    console.log(`Server running on port ${port}`);
+});
